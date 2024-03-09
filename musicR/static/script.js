63c66007d0edb5cb3a9572d46afb4eaeb1841e32
Jennifer Oreffo <57407703+jenniferoreffo@users.noreply.github.com>
2024-03-09 05:44:48 -0800

    script.js

--- musicR/static/script.js
+++ musicR/static/script.js
@@ -3,16 +3,18 @@ const form = document.getElementById("spotifyForm");
 
 // Attach an event listener to the form submission
 form.addEventListener("submit", function (event) {
-event.preventDefault(); // Prevent the default form submission
+  event.preventDefault(); // Prevent the default form submission
 
-//Get the artist name and song name inputs
-const artistNameInput = document.getElementById("artistName");
-const songNameInput = document.getElementById("songName");
+  // Get the artist name and song name inputs
+  const artistNameInput = document.getElementById("artistName");
+  const songNameInput = document.getElementById("songName");
 
-//Get the values from the input
-const artistName = artistNameInput.value;
-const songName = songNameInput.value;
+  // Get the values from the inputs
+  const artistName = artistNameInput.value;
+  const songName = songNameInput.value;
 
-// Redirect to the recommendations page with the artist name and song name as query parameters
-window.location.href = `/recommendations?artist=${encodeURIComponent(artistName)}&song=${encodeURIComponent(songName)}`;
+  // Redirect to the recommendations page with the artist name and song name as query parameters
+  window.location.href = `/recommendations?artist=${encodeURIComponent(
+    artistName
+  )}&song=${encodeURIComponent(songName)}`;
 });
